6ba7914e91301a7b09c4e3285c1d718d5056d421
Indrajit Banerjee <indrajit.banerjee25@gmail.com>
2026-09-23 00:41:05 +0530

    Sec cont responsive

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -7,11 +7,14 @@ const SecondaryContainer = () => {
   return (
     movies.nowPlayingMovies && (
       <div className="bg-black">
-        <div className=" mt-0 md:-mt-30 pl-4 md:pl-12 relative z-20">
-          <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Popular"} movies={movies.popularMovies} />
-          <MovieList title={"Top Rated"} movies={movies?.topRatedMovies} />
-          <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies} />
+        <div className="relative z-20 mt-0 px-3 sm:px-5 md:-mt-30 md:px-12">
+          <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+
+          <MovieList title="Popular" movies={movies.popularMovies} />
+
+          <MovieList title="Top Rated" movies={movies?.topRatedMovies} />
+
+          <MovieList title="Upcoming Movies" movies={movies.upcomingMovies} />
         </div>
       </div>
     )
